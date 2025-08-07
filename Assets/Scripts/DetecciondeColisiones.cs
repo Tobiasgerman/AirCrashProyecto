7fe555e5d7a6655189cec6ea5a54efdd5f06348b
TobiasGerman <tobigerman2019@gmail.com>
2025-08-07 10:30:23 -0300

    deteccion de colissiones sumado

---
+++ Assets/Scripts/DetecciondeColisiones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetecciondeColisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Contacto");
+
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
