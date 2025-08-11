5a139cb55aa14052cb1065fe1bbb8b31c49d9f70
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-08-11 09:01:05 -0300

    boton sigue sin andar

--- Assets/Scripts/Scripts Escena 1/BotonJugar.cs
+++ Assets/Scripts/Scripts Escena 1/BotonJugar.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BotonJugar : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void Escena()
     {
-        
-    }
+        SceneManager.LoadScene("escenaDos");
+        Debug.Log("bege");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
