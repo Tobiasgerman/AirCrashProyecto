40deca1688829191d21ba9f236a53d34895051f0
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-08-20 14:21:52 -0300

    luces no terminadas

---
+++ Assets/Scripts/Scripts Escena 3/LucesRojas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LucesRojas : MonoBehaviour
+{
+    public float tiempoEncendido = 0.5f;
+    public float tiempoApagado = 0.5f;
+    public Light luz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Titilar());
+    }
+
+    // Update is called once per frame
+    IEnumerator Titilar()
+    {
+        luz.enabled = true;
+        yield return new WaitForSeconds(tiempoEncendido);
+        luz.enabled = false;
+        yield return new WaitForSeconds(tiempoApagado);
+    }
+}
