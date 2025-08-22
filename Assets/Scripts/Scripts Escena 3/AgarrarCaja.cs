8b79d50c8a5bcf2c1c766792cc97337d3de30230
TobiasGerman <tobigerman2019@gmail.com>
2025-08-22 11:56:21 -0300

    continuar funcion agarrar caja

---
+++ Assets/Scripts/Scripts Escena 3/AgarrarCaja.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AgarrarCaja : MonoBehaviour
+{
+    [Header("Referencias")]
+    public GameObject jugador;   // El jugador
+    public GameObject caja;      // La caja a agarrar
+    public Button BotonCaja;     // Botón que aparece al acercarse
+    public float distanciaMax = 3f; // Distancia máxima para que aparezca el botón y se pueda agarrar
+
+    private bool agarrada = false;
+    private Rigidbody rb;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        float distancia = Vector3.Distance(jugador.transform.position, caja.transform.position);
+
+        // Mostrar u ocultar el botón según la distancia
+        if (BotonCaja != null)
+        {
+            BotonCaja.gameObject.SetActive(distancia <= distanciaMax);
+        }
+
+        // Agarrar o soltar con E si está cerca
+        if (Input.GetKeyDown(KeyCode.E) && distancia <= distanciaMax)
+        {
+            agarrada = !agarrada;
+
+            if (agarrada)
+            {
+                rb.isKinematic = true; // Desactivamos física para poder moverla
+            }
+            else
+            {
+                rb.isKinematic = false; // Activamos física para que caiga
+            }
+        }
+
+        // Si está agarrada, la caja sigue al jugador
+        if (agarrada)
+        {
+            Vector3 posicionFrente = jugador.transform.position + jugador.transform.forward * 2f + Vector3.up * 1f;
+            caja.transform.position = posicionFrente;
+        }
+    }
+}
