d5ff73dc7959e7881ed514060a1ceb6d0e29eee0
Manuel Smulovitz <49439375@est.ort.edu.ar>
2025-08-26 12:07:36 -0300

    barco demo

---
+++ Assets/Scripts/Scripts Escena Cuatro/barco.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class barco : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent Barco;
+    public Transform Destino;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Barco = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Barco.destination = Destino.position;
+        Debug.Log("Distancia al destino: " + Barco.remainingDistance);
+    }
+}
